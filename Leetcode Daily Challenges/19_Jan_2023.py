5f9758c069c121edd482bc0af184971d5fb78761
Utkarsh Chauhan <utkarshchauhan1999@gmail.com>
2023-01-19 22:58:23 +0530

    Solution for 19_Jan_2023

--- Leetcode Daily Challenges/19_Jan_2023.py
+++ Leetcode Daily Challenges/19_Jan_2023.py
@@ -2,13 +2,13 @@ class Solution(object):
     def subarraysDivByK(self, nums, k):
         sums = [0] * k
         sums[0] += 1
-        cnt = 0
+        res = 0
         currSum = 0
         for n in nums:
             currSum = (currSum + n % k + k) % k
-            cnt += sums[currSum]
+            res += sums[currSum]
             sums[currSum] += 1
-        return cnt
+        return res
 
 
 
